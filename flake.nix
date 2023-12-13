9e509034890a34598fae5138df46803d5a764959
trm109 <trm109@case.edu>
2023-12-13 03:41:12 -0500

    Updating

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
-            ./configuration.nix
+            ./hosts/default/configuration.nix
             inputs.home-manager.nixosModules.default
           ];
         };
