b8bcb445a7e23be13c170039ee2645650ee129ac
trm109 <trm109@case.edu>
2023-12-17 14:39:43 -0500

    Updating

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -67,7 +67,7 @@
     pavucontrol
     microsoft-edge
     minecraft
-
+    virtualbox
 
     # Development (CLI)
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
