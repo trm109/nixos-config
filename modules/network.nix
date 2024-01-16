f7e1849bd991f4bc7fffdab33b8ca99215b30ca3
trm109 <trm109@case.edu>
2024-01-16 14:57:50 -0500

    Updating

--- modules/network.nix
+++ modules/network.nix
@@ -5,6 +5,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.namservers = [ "8.8.8.8" "8.8.4.4" ];
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
