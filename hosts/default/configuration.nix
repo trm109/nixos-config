7655f77c176c8a4ba54d5cac6b60d22d213f03d2
trm109 <trm109@case.edu>
2024-01-16 10:52:54 -0500

    Updating

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -92,7 +92,6 @@
     # Development (CLI)
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
-    neovim
     tree
     wget
     fish
