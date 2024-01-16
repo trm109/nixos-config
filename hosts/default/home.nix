b3537598a17b25aa368504c0e5557df3f2578032
trm109 <trm109@case.edu>
2024-01-16 10:51:21 -0500

    Updating

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -95,7 +95,7 @@
     programs.neovim = 
 	let 
 	toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n"
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
 	in 
 	{
 # Enable
