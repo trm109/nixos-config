f4afed43f8524e849cb86f6fa310fe204fdd0260
trm109 <trm109@case.edu>
2024-01-17 13:42:00 -0500

    Updating

--- modules/home-manager/neovim/plugins/lazy.lua
+++ modules/home-manager/neovim/plugins/lazy.lua
@@ -58,14 +58,14 @@ require("Comment").setup({
 
 -- CMP
 require('nvim-cmp').setup({
-	snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        require('cmp_luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-      end,
+    snippet = {
+	-- REQUIRED - you must specify a snippet engine
+	expand = function(args)
+	    -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+	    require('cmp_luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+	    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	end,
     },
     window = {
       completion = cmp.config.window.bordered(),
@@ -81,11 +81,11 @@ require('nvim-cmp').setup({
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
+      --{ name = 'luasnip' }, -- For luasnip users.
       -- { name = 'ultisnips' }, -- For ultisnips users.
       -- { name = 'snippy' }, -- For snippy users.
     }, 
-	{
+    {
       { 
 		  name = 'buffer' 
 	  },
