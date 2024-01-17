9f49d0d59bd3652606f2d7b2ab1311537755d9c0
trm109 <trm109@case.edu>
2024-01-17 13:45:26 -0500

    Updating

--- modules/home-manager/neovim/plugins/lazy.lua
+++ modules/home-manager/neovim/plugins/lazy.lua
@@ -60,7 +60,7 @@ require("Comment").setup({
 -- [count]gbc
 
 -- CMP
-require('nvim-cmp').setup({
+require('cmp').setup({
     snippet = {
 	-- REQUIRED - you must specify a snippet engine
 	expand = function(args)
