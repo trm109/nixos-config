69de58d5260ecbf160bb21b2c1750617ff6045e8
trm109 <trm109@case.edu>
2024-05-11 02:18:05 -0400

    Enabled kde-connect

--- modules/software/terminal.nix
+++ modules/software/terminal.nix
@@ -32,8 +32,17 @@
 
   # Enable SSH
   services.openssh.enable = true;
-  networking.firewall.enable = false;
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+    { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+    { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  };  
   programs.ssh.forwardX11 = true;
   programs.ssh.setXAuthLocation = true;
+  programs.kdeconnect.enable = true;
   services.openssh.settings.X11Forwarding = true;
 }
