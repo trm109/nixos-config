706326ca2fbb76bd59359cbb40b4961a9b5e28a0
trm109 <trm109@case.edu>
2025-01-16 16:43:33 -0500

    Reworked hyprland packages

--- modules/software/desktop.nix
+++ modules/software/desktop.nix
@@ -30,28 +30,26 @@
   environment.systemPackages = with pkgs; [
     #(inputs.ags.packages."x86_64-linux".default.override = {})
     #ags
-    dunst
-    eww
-    stable.waybar
-    fuzzel
-    lxqt.lxqt-policykit
-    pavucontrol
-    wl-clipboard
-    libsForQt5.qt5ct
-    grim
-    slurp
-    libnotify
-    unstable.hyprlock
-    networkmanagerapplet
-    networkmanager-fortisslvpn
-    networkmanager-openconnect
-    openconnect
-    openfortivpn
-    hyprpaper
-    helvum
-    brightnessctl
-    gtk3
-    inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default
+    #dunst
+    #eww
+    #stable.waybar
+    #mako # Notifications
+    fuzzel # App launcher
+    lxqt.lxqt-policykit # Polkit
+    pavucontrol # For audio control
+    wl-clipboard # for clipboard
+    libsForQt5.qt5ct # For Qt5 config tool
+    grim # For screnshots
+    slurp # For screenshots
+    libnotify # Notify lib
+    unstable.hyprlock # For screenlocking
+    networkmanagerapplet # Applet for networkmanager
+    hyprpaper # Wallpapers
+    helvum # For piping audio
+    brightnessctl # For changing brightness
+    gtk3 # For gtk apps
+    inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default # Cursor
+    hyprpanel # Panel and notif manager
   ];
 
   xdg = {
