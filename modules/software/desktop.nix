e33caf44fe6360ca8cf326cd3a8440b41bddd21b
trm109 <trm109@case.edu>
2025-01-19 12:13:01 -0500

    tuigreet settings changed

--- modules/software/desktop.nix
+++ modules/software/desktop.nix
@@ -14,7 +14,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-user-session";
         user = "greeter";
       };
     };
