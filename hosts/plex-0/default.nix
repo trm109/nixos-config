896d88c21f16289dbda1fc3a797fbc60994b8f7b
Theo <theo@bionix.fyi>
2025-04-13 17:48:45 -0400

    moved configuration to default

---
+++ hosts/plex-0/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
