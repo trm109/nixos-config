8c057dd152443f4bf70bd71b79e68f70e2a1373d
trm109 <trm109@case.edu>
2025-08-06 14:32:24 -0500

    Opened focalboard ports

--- modules/services/homelab/focalboard.nix
+++ modules/services/homelab/focalboard.nix
@@ -9,16 +9,24 @@ in
 {
   options.modules.services.homelab.focalboard = {
     enable = lib.mkEnableOption "focalboard service";
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3128;
+      description = "Port on which Focalboard will be available.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    # Focalboard is a self-hosted project management tool from Mattermost.
     virtualisation.oci-containers.containers.focalboard = {
       image = "mattermost/focalboard:latest";
-      ports = [ "127.0.0.1:3128:8000" ];
+      ports = [ "127.0.0.1:${cfg.port}:8000" ];
       # fbdata:/opt/focalboard/data
       volumes = [
         "fbdata:/opt/focalboard/data"
       ];
     };
+    # make available on local networks.
+    networking.firewall.allowedTCPPorts = [ cfg.port ];
   };
 }
