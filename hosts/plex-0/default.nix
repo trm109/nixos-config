95e1ed246505c6e9369b9e990379d6c72a808693
trm109 <trm109@case.edu>
2025-10-16 21:21:00 -0500

    Reverting breaking change, immich seems to reject wildcard as subnet

--- hosts/plex-0/default.nix
+++ hosts/plex-0/default.nix
@@ -9,7 +9,7 @@
     openFirewall = true;
     enable = true;
     mediaLocation = "/storage/immich";
-    host = "*";
+    host = "0.0.0.0"; # "*" doesnt seem to work.
     accelerationDevices = null; # my i5-7500T is not going to accelerate anything
   };
   # Create the folder for Immich media.
