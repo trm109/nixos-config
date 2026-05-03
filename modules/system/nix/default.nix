25b56336f47ae12fe1f014323a4e2873ae6dfeba
trm109 <trm109@case.edu>
2026-05-03 17:01:52 -0500

    removing cachix substituters for better build times

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -61,14 +61,14 @@ in
         ]
         ++ users;
         substituters = [
-          "https://nix-community.cachix.org"
-          "https://hyprland.cachix.org"
-          "https://devenv.cachix.org"
+          # "https://nix-community.cachix.org"
+          # "https://hyprland.cachix.org"
+          # "https://devenv.cachix.org"
         ];
         trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          # "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         ];
       };
     };
