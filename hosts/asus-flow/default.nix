5ad2d531da64ca6f2649c5fb75a3ca9ff1553c66
trm109 <trm109@case.edu>
2026-05-31 23:31:28 -0500

    Re-enabling nvidia after GPU fan replacement

--- hosts/asus-flow/default.nix
+++ hosts/asus-flow/default.nix
@@ -4,10 +4,10 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  # hardware.nvidia.prime = {
-  #   amdgpuBusId = "PCI:10:0:0";
-  #   nvidiaBusId = "PCI:1:0:0";
-  # };
+  hardware.nvidia.prime = {
+    amdgpuBusId = "PCI:10:0:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
   modules = {
     applications = {
       #desktop.wayland.hyprland.enable = true;
@@ -15,8 +15,10 @@
     };
     firmware = {
       asus.enable = true;
-      gpu.radeon.enable = true;
-      gpu.nvidia.enable = false;
+      gpu = {
+        radeon.enable = true;
+        nvidia.enable = true;
+      };
     };
     # Hardware
     # hardware = {
